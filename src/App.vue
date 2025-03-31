c42e891cec6660290b211dcd872a1806fe172811
miguelr215 <drexelramos@gmail.com>
2025-03-30 21:13:46 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <header-navbar
+    :pages="pages"
+    :active-page="activePage"
+    :nav-link-click="(index) => (activePage = index)"
+  ></header-navbar>
+  <page-viewer :page="pages[activePage]"></page-viewer>
+</template>
+
+<script>
+import PageViewer from './components/PageViewer.vue';
+import HeaderNavbar from './components/HeaderNavbar.vue';
+
+export default {
+  components: {
+    PageViewer,
+    HeaderNavbar,
+  },
+  data() {
+    return {
+      activePage: 0,
+      pages: [
+        {
+          pageTitle: 'Home Page',
+          content: 'this is the home page',
+          link: { text: 'Home', url: 'home.html' },
+        },
+        {
+          pageTitle: 'About Page',
+          content: 'this is the about page',
+          link: { text: 'About', url: 'about.html' },
+        },
+        {
+          pageTitle: 'Contact Page',
+          content: 'this is the contact page',
+          link: { text: 'Contact', url: 'contact.html' },
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/HeaderNavbar.vue
@@ -0,0 +1,54 @@
+<template>
+  <nav :class="[`navbar-${theme}`, `bg-${theme}`, 'navbar', 'navbar-expand-lg']">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item" v-for="(page, index) in pages" :key="index">
+            <navbar-link
+              :page="page"
+              :isActive="activePage === index"
+              @click.prevent="navLinkClick(index)"
+            ></navbar-link>
+          </li>
+        </ul>
+      </div>
+      <form class="d-flex">
+        <button class="btn btn-primary" @click.prevent="changeTheme()">Toggle Dark Mode</button>
+      </form>
+    </div>
+  </nav>
+</template>
+
+<script>
+import NavbarLink from './NavbarLink.vue';
+
+export default {
+  components: { NavbarLink },
+  props: ['pages', 'activePage', 'navLinkClick'],
+  data() {
+    return {
+      theme: 'light',
+    };
+  },
+  // methods
+  methods: {
+    changeTheme() {
+      this.theme = this.theme === 'light' ? 'dark' : 'light';
+    },
+  },
+};
+</script>
+
+<style></style>
